57c7d91d500e8e869998c80f31ce291cbf466324
nasrullah123khan <93121456+nasrullah123khan@users.noreply.github.com>
2022-06-18 11:27:20 -0700

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,35 @@
+/*
+ * Filename: Time.java
+ * Author: John Kaiser
+ * Date: 2/28/2020
+ * Purpose: Create runnable Time class to get current time and update timeText textfield in GUI
+ */
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Time implements Runnable {
+    private boolean isRunning;
+    private String timePattern = "hh:mm:ss a";
+    private SimpleDateFormat timeFormat = new SimpleDateFormat(timePattern); 
+    Date date = new Date(System.currentTimeMillis());
+
+    public Time() {
+        this.isRunning = Thread.currentThread().isAlive();
+    }
+    
+    public String getTime() {
+        date = new Date(System.currentTimeMillis());
+        return timeFormat.format(date);
+    }
+
+    @Override
+    public void run() {
+        //While running, constantly update current time
+        while (isRunning) {
+            
+        	Simulator.timeText.setText(getTime());
+        } 
+    }
+    
+}
